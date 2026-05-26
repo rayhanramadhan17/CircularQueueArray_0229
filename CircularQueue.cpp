0036482332abac255a180fec9172f27bd6e3a8a2
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-05-26 11:05:50 +0700

    case 2 remove

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -149,3 +149,8 @@ int main()
                 q.insert();
                 break;
             } 
+            case '2':
+            {
+                q.remove();
+                break;
+            }
