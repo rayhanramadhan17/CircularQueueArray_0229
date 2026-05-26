40651edc5ba15f03aa1ad57fcf8e3b5f5398a680
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-05-26 11:04:46 +0700

    menampilkan program utama

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -128,4 +128,16 @@ int main()
     Queues q;
     char ch;
 
- 
+    while (true)
+    {
+        try
+        {
+            cout << "Menu" << endl;
+            cout << "1. Implement Insert Operation" << endl;
+            cout << "2. Implement Delete Operation" << endl;
+            cout << "3. Display values" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
+
