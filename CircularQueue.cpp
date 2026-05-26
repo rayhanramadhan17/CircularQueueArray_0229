a51bb2ce462466eafab6f6ccca822549a6f9d57f
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-05-26 11:06:17 +0700

    case 3 display

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -154,3 +154,8 @@ int main()
                 q.remove();
                 break;
             }
+            case '3':
+            {
+                q.display();
+                break;
+            }
