909bad679597b32c6e9bc3b7d1b8779711c5e54f
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-05-26 10:57:54 +0700

    membuat class queues

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -4,3 +4,17 @@ using namespace std;
 // CircularQueuesArray_4NIM
 // 15 commit
 
+class Queues
+{
+private:
+    static const int max = 3;
+    int FRONT, REAR;
+    int queue_array[max];
+
+public:
+    Queues()
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
+
