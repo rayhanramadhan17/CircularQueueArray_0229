dd211a7a28548db53b8e22ddfded02c83430e222
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-05-26 11:05:26 +0700

    switch case 1

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -141,3 +141,11 @@ int main()
             cin >> ch;
             cout << endl;
 
+            switch (ch)
+            {
+            case '1':
+            {
+
+                q.insert();
+                break;
+            } 
